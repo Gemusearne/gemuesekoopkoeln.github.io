759bc2e12fe00f8a063ad7d3122f0ae3380d8c5e
bkrumnow <benjamin.krumnow@th-koeln.de>
2022-11-24 18:40:08 +0100

    merge conflict

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,21 +1,25 @@
 (function(){
   function animate_slideshow(containerID) {
-    let slideIndex = 1;
+    let slideIndex = 0;
+    let ticked = false;
     const container = document.getElementById(containerID);
+    const slideClass = container.querySelector(".slide-content").className;
     run(true);
 
      onclick="plusSlides(1)"
-  
+
     // Next/previous controls
     function plusSlides(n) {
       showSlides(slideIndex += n);
+      ticked = true;
     }
 
     // Thumbnail image controls
     function currentSlide(n) {
       showSlides(slideIndex = n);
+      ticked = true;
     }
-  
+
     /**
      * Add buttons and define their functionality
      * Called once at the start. Never call it again.
@@ -25,9 +29,9 @@
       prevButton.onclick = () => plusSlides(-1);
       let nextButton = container.querySelector(".next")
       nextButton.onclick = () => plusSlides(1);
-    
+
       let contentItems = container.getElementsByClassName("slide-content");
-      let dotContainer = container.querySelector(".dots"); 
+      let dotContainer = container.querySelector(".dots");
       for (let i=1; i<=contentItems.length; i++){
         let dotSpan = document.createElement('span');
         if (i==1){
@@ -41,34 +45,37 @@
     }
 
     function showSlides(n) {
-      let i;
       let slides = container.getElementsByClassName("slide-content");
       let dots = container.getElementsByClassName("dot");
+      if (container.querySelector(".fade-in") != null){
+        container.querySelector(".fade-in").className = slideClass + " fade-out";
+      }
       if (n > slides.length) {slideIndex = 1}
       if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      for (i = 0; i < dots.length; i++) {
+      slides[slideIndex-1].className = slideClass + " fade-in";
+
+      for (let i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
       }
-      slides[slideIndex-1].style.display = "block";
       dots[slideIndex-1].className += " active";
-    } 
+    }
 
     function run(first){
       if (first){
         setTimeout(()=>{
+          slideIndex++;
           setButtons(container);
           showSlides(1);
-          slideIndex++;
         }, 50);
       }
       setTimeout(()=>{
-          showSlides(slideIndex);
-          slideIndex++;
-          run();
-        }, 5*1000);
+        if (!ticked) {
+            slideIndex++;
+            showSlides(slideIndex);
+        }
+        run();
+        ticked = false;
+      }, 5*1000);
     }
   }
 
